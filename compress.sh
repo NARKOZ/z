e87b09c28fadcf0a318d846c24a8bad83b1d1a18
disassemblyline <somicide@gmail.com>
2011-06-15 22:28:32 -0400

    update compress.sh script

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-yuicompressor -o public/css/z.min.css public/css/z.css
-yuicompressor -o public/js/z.min.js public/js/z.js
+java -jar ./yuicompressor.jar -o public/css/z.min.css public/css/z.css
+java -jar ./yuicompressor.jar -o public/js/z.min.js public/js/z.js
