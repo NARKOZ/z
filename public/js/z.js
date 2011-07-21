4a403d3f0552acccdfc3dbc38783190e4b141c8e
disassemblyline <somicide@gmail.com>
2011-07-21 16:15:49 -0400

    undo debugging

--- public/js/z.js
+++ public/js/z.js
@@ -1496,12 +1496,12 @@ function z_engine_stream_queue()
 /* timers */
 function z_engine_timers()
 {
-	/*z_engine_fetch_timeline("rates");
+	z_engine_fetch_timeline("rates");
 	z_engine_fetch_timeline("home");
 	z_engine_fetch_timeline.delay(2, "userstream");
 	z_engine_fetch_timeline.delay(15, "mentions");
 	z_engine_fetch_timeline.delay(30, "dms-inbox");
-	z_engine_fetch_timeline.delay(45, "dms-outbox");*/
+	z_engine_fetch_timeline.delay(45, "dms-outbox");
 	new PeriodicalExecuter(function()
 	{
 		z_engine_update_relative_time("time.home");
